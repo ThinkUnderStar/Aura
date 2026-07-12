7eda88ea11fc50a2165f9f9cc80021b2ade9f86e
Jiang <185388966@qq.com>
2026-07-12 16:12:56 +0800

    Aura项目开工

---
+++ Aura-backend-server/src/main/java/thinkunderstar/aura/aurabackendserver/AuraBackendServerApplication.java
@@ -0,0 +1,13 @@
+package thinkunderstar.aura.aurabackendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraBackendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraBackendServerApplication.class, args);
+    }
+
+}
---
+++ Aura-backend-server/src/test/java/thinkunderstar/aura/aurabackendserver/AuraBackendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package thinkunderstar.aura.aurabackendserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraBackendServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
